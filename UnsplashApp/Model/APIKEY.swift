b2ffdeeb059ddc8ee22b171b7d194422167ea43b
KEEN <homurahomu@naver.com>
2021-06-27 13:49:12 +0900

    apikey 관련 설정

---
+++ UnsplashApp/Model/APIKEY.swift
@@ -0,0 +1,20 @@
+//
+//  APIKEY.swift
+//  UnsplashApp
+//
+//  Created by KEEN on 2021/06/27.
+//
+
+import Foundation
+
+extension Bundle {
+  var apikey: String {
+    guard let file = self.path(forResource: "APIKEY", ofType: "plsit") else { return "" }
+    
+    guard let resource = NSDictionary(contentsOfFile: file) else { return "" }
+    guard let key = resource["API_KEY"] as? String else {
+      fatalError("APIKEY.plist에 API_KEY를 설정해주세요.")
+    }
+    return key
+  }
+}
